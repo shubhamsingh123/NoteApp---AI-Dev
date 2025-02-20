8896118571f26e04223bb54f299e53f82c346307
shubhamsingh123 <shubham16cse06@gmail.com>
2025-02-20 13:53:28 +0530

    Empty branch

--- src/main/java/com/telus/demo/exception/NoteNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.telus.demo.exception;
-
-public class NoteNotFoundException extends RuntimeException {
-    public NoteNotFoundException(String message) {
-        super(message);
-    }
-}
